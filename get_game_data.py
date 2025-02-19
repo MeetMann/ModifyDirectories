653e97dd89a41ce1a51e987c81f80fe077989afb
Meet <mann.prabhmeet@gmail.com>
2025-02-18 20:02:31 -0400

    Completed python scripting mini-project!

--- get_game_data.py
+++ get_game_data.py
@@ -4,82 +4,65 @@ import shutil
 from subprocess import PIPE, run
 import sys
 
-
 GAME_DIR_PATTERN = "game"
-GAME_CODE_EXTENSION = ".go"
-GAME_COMPILE_COMMAND = ["go", "build"]
+mappings = {}
 
+def save_mappings(mappings , filename = "mappings.json"):
+    with open(filename, "w") as f: 
+        json.dump(mappings, f)
+
+def get_mappings(filename = "mappings.json"):
+    if (os.path.exists(filename)):
+        with open(filename, "r") as f:
+            return json.load(f)
+    return {}
 
 def find_all_game_paths(source):
     game_paths = []
 
     for root, dirs, files in os.walk(source):
         for directory in dirs:
-            if GAME_DIR_PATTERN in directory.lower():
+            if GAME_DIR_PATTERN in directory:
                 path = os.path.join(source, directory)
                 game_paths.append(path)
-
         break
 
     return game_paths
 
 
-def get_name_from_paths(paths, to_strip):
+def remove_part_of_paths(paths, remove_this):
     new_names = []
+
     for path in paths:
         _, dir_name = os.path.split(path)
-        new_dir_name = dir_name.replace(to_strip, "")
+        new_dir_name = dir_name.replace(remove_this, "")
+        if new_dir_name[-1] == "_":
+            new_dir_name = new_dir_name[:-1]
         new_names.append(new_dir_name)
-
+    
     return new_names
 
+def move_games_to_new_dir(game_paths, game_dirs, target_path):
+    for src, dest in zip(game_paths, game_dirs):
+        new_path = os.path.join(target_path, dest)
+        mappings[new_path] = src
+
+        if (os.path.exists(new_path)):
+            shutil.rmtree(new_path)
+        
+        shutil.move(src, new_path)
+        save_mappings(mappings)
+
+def move_games_back(mappings):
+    for src, dest in mappings.items():
+        if (os.path.exists(dest)):
+            continue
+        shutil.move(src, dest)
+    os.remove("mappings.json")
 
 def create_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
-
-
-def copy_and_overwrite(source, dest):
-    if os.path.exists(dest):
-        shutil.rmtree(dest)
-    shutil.copytree(source, dest)
-
-
-def make_json_metadata_file(path, game_dirs):
-    data = {
-        "gameNames": game_dirs,
-        "numberOfGames": len(game_dirs)
-    }
-
-    with open(path, "w") as f:
-        json.dump(data, f)
-
-
-def compile_game_code(path):
-    code_file_name = None
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(GAME_CODE_EXTENSION):
-                code_file_name = file
-                break
-
-        break
-
-    if code_file_name is None:
-        return
-
-    command = GAME_COMPILE_COMMAND + [code_file_name]
-    run_command(command, path)
-
-
-def run_command(command, path):
-    cwd = os.getcwd()
-    os.chdir(path)
-
-    result = run(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
-    print("compile result", result)
-
-    os.chdir(cwd)
 
 def main(source, target):
     cwd = os.getcwd()
@@ -87,23 +70,24 @@ def main(source, target):
     target_path = os.path.join(cwd, target)
 
     game_paths = find_all_game_paths(source_path)
-    new_game_dirs = get_name_from_paths(game_paths, "_game")
+    new_game_dirs = remove_part_of_paths(game_paths, GAME_DIR_PATTERN)
 
     create_dir(target_path)
 
-    for src, dest in zip(game_paths, new_game_dirs):
-        dest_path = os.path.join(target_path, dest)
-        copy_and_overwrite(src, dest_path)
-        compile_game_code(dest_path)
-
-    json_path = os.path.join(target_path, "metadata.json")
-    make_json_metadata_file(json_path, new_game_dirs)
-
+    if (os.path.exists("mappings.json")):
+        mappings = get_mappings()
+        move_games_back(mappings)
+    else:
+        move_games_to_new_dir(game_paths, new_game_dirs, target_path)
+    
 
 if __name__ == "__main__":
     args = sys.argv
-    if len(args) != 3:
-        raise Exception("You must pass a source and target directory - only.")
 
+    if len(args) != 3:
+        raise Exception("You must pass a source and target directory - no more, no less")
+    
     source, target = args[1:]
     main(source, target)
+
+
